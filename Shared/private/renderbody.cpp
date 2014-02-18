0b58a03913a493043035503960ead267f642e66f
Corey Waugh <corey.m.waugh@gmail.com>
2014-02-18 00:49:17 +0000

    Name: Corey Waugh and Eugene Davis Date: 2/17/2014 Created the render body class source and header files.
    git-svn-id: svn+ssh://blackhawk.ece.uah.edu/repos/cpe453/team8/pixelspace/branches/develop@50 1666c064-e9dd-4f10-ba9e-9d013ce9a313

--- Shared/private/renderbody.cpp
+++ Shared/private/renderbody.cpp
@@ -1,6 +1,38 @@
 #include "renderbody.h"
 
-RenderBody::RenderBody() :
+RenderBody::RenderBody(QString spritePath) :
     Body()
+{
+    this->setSpritePath(spritePath);
+    createPixmap(); // this will load the graphic from spritePath
+
+}
+
+void RenderBody::render()
+{
+
+
+}
+
+void RenderBody::createPixmap()
+{
+    //load pixmap from spritePath
+    sprite = new QPixmap();
+    sprite->load(spritePath);
+
+}
+
+void RenderBody::tick(double)
+{
+
+}
+
+
+void RenderBody::setSpritePath(QString spritePath)
+{
+    this->spritePath = spritePath;
+}
+
+void RenderBody::collisionOccurred(RenderBody)
 {
 }
--- Shared/private/renderbody.h
+++ Shared/private/renderbody.h
@@ -2,11 +2,22 @@
 #define RENDERBODY_H
 
 #include "body.h"
+#include <QPixmap>
 
 class RenderBody : public Body
 {
 public:
-    RenderBody();
+    RenderBody(QString spritePath);
+    void render();
+    void createPixmap();
+    void tick(double);
+    void setSpritePath(QString spritePath);
+    void collisionOccurred(RenderBody);
+
+private:
+    QString spritePath;
+    QPixmap *sprite;
+
 };
 
 #endif // RENDERBODY_H

e94f3a49b80c8a76bc648d41f6729affb9c623d1
Jacob Swanson <jacob@swandiggy.com>
2014-02-19 20:06:03 +0000

    Some work into loading sprite from DB. Jacob/Eugene
    git-svn-id: svn+ssh://blackhawk.ece.uah.edu/repos/cpe453/team8/pixelspace/branches/develop@56 1666c064-e9dd-4f10-ba9e-9d013ce9a313

--- Client/helper.cpp
+++ Client/helper.cpp
@@ -16,21 +16,24 @@ Helper::Helper()
     SimpleDataManager dm;
     if (dm.loadBodySprite(QString("a"), &sprite))
     {
-        qDebug() << sprite;
+       // qDebug() << sprite;
     }
     else
     {
         qDebug() << "Could not load sprite";
     }
 
-    //ship = QPixmap(ship_xpm);
+    QByteArray ba = sprite.toLocal8Bit();
+    const char *ship_xpm = ba.data();
+    this->ship = QPixmap(ship_xpm);
+    qDebug() << ship;
 }
 
 void Helper::paint(QPainter *painter, QPaintEvent *event, int elapsed)
 {
     painter->save();
     // Draw stuff
-    painter->fillRect(event->rect(), background);
+    //painter->fillRect(event->rect(), background);
 
     int shipx = 50;
     int shipy = 50;
@@ -40,13 +43,13 @@ void Helper::paint(QPainter *painter, QPaintEvent *event, int elapsed)
     int cy = sizey - shipy;
 
     // Center on ship
-//    QTransform transform;
-//    transform.translate(cx, cy);
-//    painter->setWorldTransform(transform);
+    QTransform transform;
+    //transform.translate(cx, cy);
+    //painter->setWorldTransform(transform);
 
-//    // Draw ship
-//    painter->translate(shipx - this->ship.width()/2, shipy - this->ship.height()/2);
-//    painter->drawPixmap(0, 0, this->ship);
+    // Draw ship
+    //painter->translate(shipx - this->ship.width()/2, shipy - this->ship.height()/2);
+    painter->drawPixmap(200, 200, this->ship);
 
     painter->restore();
 }
